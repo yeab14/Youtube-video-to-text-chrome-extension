abebb56b7671835cf08683c15bc682e40a147c3b
yeab14 <155532389+yeab14@users.noreply.github.com>
2024-06-16 01:36:41 -0700

    transcription error

--- Streamlit_App/main.py
+++ Streamlit_App/main.py
@@ -13,6 +13,7 @@ origins = [
     "*",
 ]
 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -56,5 +57,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
-
 
